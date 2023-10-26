2a4d43b178914d622577b53dc699ca2edbe646d5
Lexsak <bartoszpiprek123@gmail.com>
2023-10-26 19:33:06 +0200

    Business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <button type='button'
+      className={`
+    py-4 px-6  mt-10
+    bg-blue-gradient 
+    font-poppins font-medium 
+    text-[18px] text-primary outline-none 
+    rounded-[5px]
+    transition-element
+    hover:scale-90`}>
+      Get Started
+    </button>
   )
 }
 
--- src/components/GetStarded.jsx
+++ src/components/GetStarded.jsx
@@ -4,7 +4,7 @@ import { arrowUp } from '../assets'
 
 const GetStarded = () => {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer transition-element hover:scale-90`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full `}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-popins font-medium text-[18px] leading-[23px] mr-2'>
