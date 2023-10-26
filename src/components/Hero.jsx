7afc026b99e89b9c78cd3fa4fc445c0626146bc9
Lexsak <bartoszpiprek123@gmail.com>
2023-10-26 18:39:15 +0200

    Stats section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,7 @@ import styles from '../style';
 import { discount, robot } from '../assets';
 import GetStarted from '../components/GetStarded'
 
-const Hero = () => {
-  return (
+const Hero = () =>(
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
 
@@ -47,9 +46,9 @@ const Hero = () => {
       {/* Hero Robot Image */}
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="billing" className='w-full h-full relative z-[5]' />
-        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+        {/* <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
         <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
-        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' /> */}
       </div>
 
       {/* Get Started on mobile device */}
@@ -59,6 +58,6 @@ const Hero = () => {
 
     </section>
   )
-}
+
 
 export default Hero
