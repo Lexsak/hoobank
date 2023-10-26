30afaccfe00feefe33f3ff338c8003a3390322b1
Lexsak <bartoszpiprek123@gmail.com>
2023-10-26 18:26:35 +0200

    Hero section

--- src/components/GetStarded.jsx
+++ src/components/GetStarded.jsx
@@ -5,7 +5,7 @@ import { arrowUp } from '../assets'
 const GetStarded = () => {
   return (
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-      <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full transition ease-in-out delay-150 hover:opacity-90`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full `}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-popins font-medium text-[18px] leading-[23px] mr-2'>
             <span className='text-gradient'>Get</span>
