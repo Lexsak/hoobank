cc75692a8ef9340a4ebec8c00af83558487acb16
Lexsak <bartoszpiprek123@gmail.com>
2023-10-27 13:09:30 +0200

    Clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from '../style'
+import { clients } from '../constants'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}>
+            <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain cursor-pointer transition-element hover:scale-90" />
+          </div>
+        ))
+        }
+      </div>
+
+    </section>
   )
 }
 
